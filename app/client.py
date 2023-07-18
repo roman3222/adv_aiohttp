e76f27202f63716fc2325fd0ff8e17e115fceea9
roman3222 <romkakms3222@gmail.com>
2023-07-18 12:37:03 +1000

    change mistake

--- app/client.py
+++ app/client.py
@@ -6,7 +6,7 @@ async def main():
     async with aiohttp.ClientSession() as session:
         response = await session.post(
             "http://0.0.0.0:8080/users/",
-            json={"name": "hgfdgfd", "password": "154354l5lk", "email": "g5uuuuuu@gmail.com"}
+            json={"name": "roman7887", "password": "fhdjh43kj43hj", "email": "g5uuuuuu@gmail.com"}
         )
         json_data = await response.text()
         print(json_data)
