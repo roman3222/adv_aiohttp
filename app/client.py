bf36b589b3d6b60a704af36df22195c8e737a556
roman3222 <romkakms3222@gmail.com>
2023-07-18 13:27:40 +1000

    add ignore

--- app/client.py
+++ app/client.py
@@ -6,7 +6,7 @@ async def main():
     async with aiohttp.ClientSession() as session:
         response = await session.post(
             "http://0.0.0.0:8080/users/",
-            json={"name": "ghfhgkj58955", "password": "bfdfbdnfbd44", "email": "g5uuuuuu@gmail.com"}
+            json={"name": "g6565h6jk5h", "password": "bfdfbdnfbd44", "email": "g5uuuuuu@gmail.com"}
         )
         json_data = await response.text()
         print(json_data)
